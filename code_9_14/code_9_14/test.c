3d89796735b8212780e4a193c6bc4464a9048b18
JOJO <87429954+ChenJHchen@users.noreply.github.com>
2024-09-14 01:14:05 +0800

    Add files via upload

---
+++ code_9_14/code_9_14/test.c
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+//int main()
+//{
+//	float h = 0;
+//	float w = 0;
+//	float area = 0;
+//	h = 10.5;
+//	w = 20.5;
+//	printf("area  = %6.2f\n", (h * w));
+//	
+//	return 0;
+//}
+
+//float area(double x,double y)
+//{
+//	return x * y;
+//}
+//int main()
+//{
+//	double h1 = 0;
+//	double h2 = 0;
+//	double w1 = 0;
+//	double w2 = 0;
+//	double s1 = 0;
+//	double s2 = 0;
+//	h1 = 10.5;
+//	w1 = 20.5;
+//	h2 = 1.5 * h1;
+//	w2 = 1.5 * w1;
+//	s1 = area(h1, w1);
+//	s2 = area(h2, w2);
+//	printf("area = %6.2f\n", s1 + s2);
+//	return 0;
+//}
+
+
+int main()
+{
+	float r = 0;
+	double s = 0;
+	r = 15.5;
+	s = 2 * 3.14 * r;
+	printf("r = %4.2f\ns = %4.2lf\n", r, s);
+	return 0;
+}
