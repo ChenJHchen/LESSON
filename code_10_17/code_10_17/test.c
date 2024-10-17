ec7acdd01a62c30c202f32694f50052c123f1578
JOJO <87429954+ChenJHchen@users.noreply.github.com>
2024-10-18 00:02:38 +0800

    Add files via upload

---
+++ code_10_17/code_10_17/test.c
@@ -0,0 +1,263 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+//int main()
+//{
+//	int code = 0;
+//	scanf("%d", &code);
+//	if (code == 3)
+//	{
+//		printf("hehe\n");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//    int a = 0;
+//    int b = 2;
+//    if (a == 1)
+//        if (b == 2)
+//            printf("hehe\n");
+//        else
+//            printf("haha\n");
+//    return 0;
+//}
+
+
+//int main()
+//{
+//    int a = 0;
+//    int b = 2;
+//    if (a == 1)
+//    {
+//        if (b == 2)
+//        {
+//            printf("hehe\n");
+//        }
+//    }
+//    else
+//    {
+//        printf("haha\n");
+//    }
+//    return 0;
+//}
+
+
+
+//int main()
+//{
+//	int num = 0;
+//	scanf("%d", &num);
+//	if (num % 2   == 1)
+//	{
+//		printf("%d is 奇数\n", num);
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i < +100; i+=2)
+//	{
+//		printf("%d ", i);
+//	}
+//	
+// 
+// 
+// 
+// return 0;
+//}
+
+
+
+
+//int main()
+//{
+//    int n = 1;
+//    
+//    int m = 2;
+//    switch (n)
+//    {
+//    case 1:
+//        m++;
+//    case 2:
+//        n++;
+//    case 3:
+//        switch (n)
+//        {//switch允许嵌套使用
+//        case 1:
+//            n++;
+//        case 2:
+//            m++;
+//            n++;
+//            break;
+//        }
+//    case 4:
+//        m++;
+//        break;
+//    default:
+//        break;
+//    }
+//    printf("m = %d, n = %d\n", m, n);
+//    return 0;
+//}
+
+
+
+//
+//int main()
+//{
+//	int i = 0;
+//	while(i<10)
+//	{
+//		i++;
+//		printf("%d\n", i);
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 1;
+//	while (i <= 10)
+//	{
+//		if (i == 5)
+//			break;
+//			printf("%d ", i);
+//		i = i + 1;
+//	}
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int i = 1;
+//	while (i <= 10)
+//	{
+//		if (i == 5)
+//			continue;
+//		printf("%d ", i);
+//		i = i + 1;
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//    int i = 1;
+//    while (i <= 10)
+//    {
+//        i = i + 1; 
+//     
+//            if (i == 5)
+//                continue;
+//        printf("%d ", i);//2 3 4 6 7 8 9 10 11
+//    }
+//    return 0;
+//}
+
+
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <=10; i++)
+//	{
+//		printf("%d\n", i);
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 10; i++)
+//	{
+//		if (i == 5)
+//			break;
+//		printf("%d ", i);
+//	}
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 10; i++)
+//	{
+//		if (i == 5)
+//			continue;
+//		printf("%d ", i);
+//	}
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	//代码1
+//	//for (;;)
+//	//{
+//	//	printf("hehe\n");
+//	// 
+//	//}
+//
+//
+//	
+//	int i = 0;
+//	int j = 0;
+//	//如果省略掉初始化部分，这里打印多少个hehe?
+//	for (; i < 10; i++)
+//	{
+//		for (; j < 10; j++)
+//		{
+//			printf("hehe\n");
+//		}
+//	}
+//
+//	return 0;
+//}
+//
+
+
+//int main()
+//{
+//    int i = 1;
+//
+//    do
+//    {
+//        if (5 == i)
+//            break;
+//        printf("%d\n", i);
+//    } while (i < 10);
+//
+//    return 0;
+//}
+//
+
+
+//int main()
+//{
+//    int i = 1;
+//
+//    do
+//    {
+//        if (5 == i)
+//            continue;
+//        printf("%d\n", i);
+//    } while (i < 10);
+//
+//    return 0;
+//}
+
